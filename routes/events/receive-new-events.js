8d6208e576af853722778fd4309dc0f28a72ab21
akamaozu <uzo@designbymobius.ca>
2016-09-30 03:08:12 -0400

    identify repo push events

--- routes/events/receive-new-events.js
+++ routes/events/receive-new-events.js
@@ -1,10 +1,24 @@
 var receive_new_event_route = module.exports = require('express').Router();
 
-receive_new_event_route.post('/', require('body-parser')() );
+// parse http form post
+  receive_new_event_route.post('/', require('body-parser')() );
 
-receive_new_event_route.post('/', function( req, res ){
+// POST /
+// parse submodule repo event 
+  receive_new_event_route.post('/', function( req, res ){
 
-  console.log( req.body );
+    // determine event type
 
-  res.sendStatus( 200 );
-});
+      // pull request
+        if( req.body.push ){
+
+          var update = req.body.push;
+
+          console.log( 'submodule repo updated!\n\n', update );
+        }
+
+      // unknown
+        else console.log( 'unknown submodule repo event\n\n', req.body );
+
+    res.sendStatus( 200 );
+  });
